c861a89137acbb3cdec42e99b912fa556236c857
jeffgrunewald <jeff@grunewalddesign.com>
2019-12-11 03:21:29 -0500

    deploy and licensing setup

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule SaltNPepa.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/jeffgrunewald/salt_n_pepa",
+      dialyzer: [plt_file: {:no_warn, ".plt/dialyzer.plt"}]
     ]
   end
 
@@ -19,8 +24,30 @@ defmodule SaltNPepa.MixProject do
 
   defp deps do
     [
+      {:credo, "~> 1.1", only: :dev, runtime: false},
       {:gen_stage, "~> 0.14.0"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:ex_doc, "~> 0.21.0", only: :dev},
+    ]
+  end
+
+  defp description(), do: "An Elixir library for creating data ingestion push gateways."
+
+  defp package() do
+    [
+      maintainers: ["jeffgrunewald"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jeffgrunewald/salt_n_pepa"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      source_url: "https://github.com/jeffgrunewald/salt_n_pepa",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
